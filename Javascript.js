58d726836393703dc88191a1e4d90c395140fcbc
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-08 00:20:29 +0530

    About_alert()

--- Javascript.js
+++ Javascript.js
@@ -2,7 +2,5 @@
 num1 = prompt("Enter 1st real/floating-point number: ");
 num2 = prompt("Enter 1st real/floating-point number: ");
 //String to float conversion
-alert("The sum of real numbers is "+ (parseFloat(num1) + parseFloat(num2)));
-
-console.log(typeof num1); // by default, prompt considers the i/p as a string.
-console.log(num1 + 9); // It gives error as concatenation cannot be done b/w number and string.
+sum = (parseFloat(num1) + parseFloat(num2));
+prompt("The sum of real numbers is "+ sum);
