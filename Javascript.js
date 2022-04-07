c09188202ff73079e31862470300645aa106f372
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-08 00:28:49 +0530

    Operators

--- Javascript.js
+++
@@ -1,6 +0,0 @@
-//Explicit Conversions
-num1 = prompt("Enter 1st real/floating-point number: ");
-num2 = prompt("Enter 1st real/floating-point number: ");
-//String to float conversion
-sum = (parseFloat(num1) + parseFloat(num2));
-prompt("The sum of real numbers is "+ sum);
