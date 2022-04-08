fba72da27a152a36402eefddea7ce0796839ff0c
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-09 02:48:38 +0530

    JSON-Obj_Creation

--- Javascript.js
+++ Javascript.js
@@ -1,9 +1,41 @@
-//closure
-var add = (function() {
-    var counter = 0;
-    return function() {
-        return counter += 1;
-         
-    }
-}) ();
+//method 1,2
+function createPerson() {
+    var person = new Object(); // or {}; 
+    person.name = "Prathyusha";
+    person.designation = "B.Tech";
+    person.phno = 1234567890;
+    return person;
+}
 
+var a = createPerson();
+console.log(a);
+console.log(a.name);
+/*
+console.log(createPerson.name); //createPerson
+console.log(person.name); //error
+*/
+
+//method 3
+function createPerson3() {
+    var person = {}; 
+    person['name'] = "Prathyusha";
+    person['phno'] = 1234567890;
+    return person;
+}
+
+var b = createPerson3();
+console.log(b);
+console.log(b.name);
+
+//method 4
+function createPerson4(){
+    var person = 
+    {
+        name:"Prathyusha",
+        age:20
+    };
+    return person;
+}
+
+var c = createPerson4();
+console.log(c.name);
