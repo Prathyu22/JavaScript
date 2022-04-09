665d4e7a94a2f4c6502ed16901fda33d870abc95
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-09 14:40:53 +0530

    screen_object

--- Javascript.js
+++ Javascript.js
@@ -1,3 +1,4 @@
-var a = window.open("https://google.com","newWindow","height=200,width=100");
-a.moveTo(500,500);
-a.focus();
+console.log(screen.height);
+console.log(screen.availHeight);
+console.log(screen.width);
+console.log(screen.availWidth);
