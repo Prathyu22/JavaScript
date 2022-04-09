276884192b51c30da9aea33f7914945b368881fe
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-09 18:01:00 +0530

    BOM

--- Javascript.js
+++
@@ -1,5 +0,0 @@
-document.write("<b>Href of location:</b> "+location.href);
-document.write("<br> <b>Protocol</b>: "+location.protocol);
-document.write("<br> <b>PathName</b>: "+location.pathname);
-document.write("<br> <b>assignValue</b>: "+location.assign);
-document.write("<br> <b>HostName</b>: "+location.hostName);
