d214dfeaf44d828d5aff20d37683b0a0440a5ede
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-09 15:10:48 +0530

    location_object

--- Javascript.js
+++ Javascript.js
@@ -1,6 +1,5 @@
-document.write("<br> <b>AppCodeName:</b> "+navigator.appCodeName);
-document.write("<br> <b>AppName:</b> "+navigator.appName);
-document.write("<br> <b>Platform:</b> "+navigator.platform);
-document.write("<br> <b>Whether cookie is enabled/not ?</b> "+navigator.cookieEnabled);
-document.write("<br> <b>Product:</b> "+navigator.product);
-document.write("<br> <b>UserAgent:</b> "+navigator.userAgent);
+document.write("<b>Href of location:</b> "+location.href);
+document.write("<br> <b>Protocol</b>: "+location.protocol);
+document.write("<br> <b>PathName</b>: "+location.pathname);
+document.write("<br> <b>assignValue</b>: "+location.assign);
+document.write("<br> <b>HostName</b>: "+location.hostName);
