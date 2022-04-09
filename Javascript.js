64a5ac5ae97b125d12135efba53e2e936f34632d
Veturi Lakshmi Prathyusha <lakshmiprathyushaveturi@gmail.com>
2022-04-09 14:59:21 +0530

    navigator_object

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,6 @@
-console.log(screen.height);
-console.log(screen.availHeight);
-console.log(screen.width);
-console.log(screen.availWidth);
+document.write("<br> <b>AppCodeName:</b> "+navigator.appCodeName);
+document.write("<br> <b>AppName:</b> "+navigator.appName);
+document.write("<br> <b>Platform:</b> "+navigator.platform);
+document.write("<br> <b>Whether cookie is enabled/not ?</b> "+navigator.cookieEnabled);
+document.write("<br> <b>Product:</b> "+navigator.product);
+document.write("<br> <b>UserAgent:</b> "+navigator.userAgent);
